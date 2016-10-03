fb649edfc9c7bac2ceb79243d413ce83cbb91ea2
rokoncse <rokoncsemail@gmail.com>
2016-10-03 21:12:17 +0600

    add multiplex site

---
+++ js/custom.js
@@ -0,0 +1,26 @@
+
+
+    $(document).ready(function() {
+     
+    
+	  
+	  
+		$("#owl-demo").owlCarousel({
+		 
+			autoPlay: 3000, //Set AutoPlay to 3 seconds
+			
+			items : 6,
+			itemsDesktop : [1199,3],
+			itemsDesktopSmall : [979,3]
+		 
+		});
+		
+		
+		
+		$('.carousel').carousel({
+		  interval: 3000
+		})
+			 
+    });
+
+
